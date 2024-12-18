0c6b99441f5bca1942fd6bcb0e3744d90b6655ed
Ricardo André Zampiva Teixeira <ricardo@pidigital.dev>
2024-12-18 15:07:43 -0300

    Imprime porcentagem e tempo restante

--- src/services/worker.js
+++ src/services/worker.js
@@ -1,10 +1,16 @@
 const { parentPort, workerData } = require('worker_threads');
 const sleep = require('./sleep');
+let calcAuxSize = 0;
+let incrementSize = 0;
+
 // const { sendWhatsApp } = require('./sendWhatsAppModule'); // Caminho para o módulo onde `sendWhatsApp` está exportado
 
 (async () => {
     try {
         const { numbers, message, image, interval } = workerData;
+
+        calcAuxSize = 100 / numbers.length;
+
         for (const phone of numbers) {
             try {
                 // await sendWhatsApp(phone, message, image);
@@ -12,6 +18,11 @@ const sleep = require('./sleep');
             } catch (error) {
                 console.error(`Erro ao enviar mensagem para ${phone}: ${error.message}`);
             }
+
+            incrementSize++;
+
+            // Imprime a porcentagem do envio e tempo restante no console
+            console.log(Math.floor((calcAuxSize * incrementSize)) + "% Tempo restante: " + ((numbers.length - incrementSize) * (interval / 1000) + "s"));
 
             await sleep(interval);
         }
